85b67e9081f0630d5711ff782b6a3fbd6308db1f
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-11-02 23:25:51 +0100

    forms

--- introduction/hello-world/src/App.js
+++ introduction/hello-world/src/App.js
@@ -1,42 +1,65 @@
-import logo from './logo.svg';
-import React, {Component} from 'react';
-import './App.css';
-import MyGreet from './components/Greet';
-import Welcome from './components/Welcome';
-import Hello from './components/Hello';
-import Message from './components/Message';
-import Counter from './components/Counter';
-import FunctionClick from './components/FunctionClick';
-import ClassClick from './components/ClassClick';
-import EventBind from './components/EventBind';
-import ParentComponent from './components/ParentComponent';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
-import Stylesheet from './components/Stylesheet';
-function App() {
-  return (
-    <div className="App" >
-      <Stylesheet/>
-    {/* <NameList/> */}
-      {/* <UserGreeting/> */}
-      {/* <ParentComponent/> */}
-      {/* <EventBind/> */}
-      {/* <FunctionClick />
-      <ClassClick/> */}
-      {/* <Counter/> */}
-      {/* <Message /> */}
-      {/* <MyGreet name="Bruce" heroName="Batman">
-        <p>THis is children props</p>
-      </MyGreet>
-      <MyGreet name="Clark" heroName="Superman">
-        <button>Action</button>
-      </MyGreet>
+// import logo from './logo.svg';
+// import React, {Component} from 'react';
+// import './App.css';
+// import MyGreet from './components/Greet';
+// import Welcome from './components/Welcome';
+// import Hello from './components/Hello';
+// import Message from './components/Message';
+// import Counter from './components/Counter';
+// import FunctionClick from './components/FunctionClick';
+// import ClassClick from './components/ClassClick';
+// import EventBind from './components/EventBind';
+// import ParentComponent from './components/ParentComponent';
+// import UserGreeting from './components/UserGreeting';
+// import NameList from './components/NameList';
+// import Stylesheet from './components/Stylesheet';
+// import Inline from './components/Inline';
+// import './appStyles.css'
+// import styles from './appStyles.module.css'
+// import Form from './components/Form'
 
-      <Welcome name="Bruce" heroName="Batman"/>
-      <Welcome name="Clark" heroName="Superman"/> */}
-      {/* <Hello/> */}
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App" >
+//       <h1 className='error'>Error</h1>
+//       <h1 className={styles.success}>Success</h1>
+//       {/* <Inline/> */}
+//       {/* <Stylesheet primary={true}/> */}
+//     {/* <NameList/> */}
+//       {/* <UserGreeting/> */}
+//       {/* <ParentComponent/> */}
+//       {/* <EventBind/> */}
+//       {/* <FunctionClick />
+//       <ClassClick/> */}
+//       {/* <Counter/> */}
+//       {/* <Message /> */}
+//       {/* <MyGreet name="Bruce" heroName="Batman">
+//         <p>THis is children props</p>
+//       </MyGreet>
+//       <MyGreet name="Clark" heroName="Superman">
+//         <button>Action</button>
+//       </MyGreet>
+
+//       <Welcome name="Bruce" heroName="Batman"/>
+//       <Welcome name="Clark" heroName="Superman"/> */}
+//       {/* <Hello/> */}
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import React, {Component} from 'react'
+import './App.css'
+import Form from './components/Form'
+
+class App extends Component{
+  render(){
+    return(
+      <div className='App'>
+       <Form/>
+      </div>
+    )
+  }
 }
-
 export default App;
--- introduction/hello-world/src/components/Stylesheet.js
+++ introduction/hello-world/src/components/Stylesheet.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import './myStyles.css'
 
-function Stylesheet() {
+function Stylesheet(props) {
+  let className = props.primary ? 'primary' : ''
   return (
     <div>
-      <h1>Stylesheets</h1>
+      <h1 className={`${className} font-xl`}>Stylesheets</h1>
     </div>
   )
 }
