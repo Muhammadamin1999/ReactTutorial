ec7a285c4e4d1ae2e742f68f757c193707a983b3
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2023-11-14 23:37:56 +0100

    react basics

--- introduction/hello-world/src/components/Greet.js
+++ introduction/hello-world/src/components/Greet.js
@@ -1,20 +1,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
+function Greet(){
+    return <h1>Hello World</h1>
+}  
 
-// function Greet(){
-//     return <h1>Hello World</h1>
-// }
-const Greet = props => {
-  const {name, heroName} = props;
-    return(
-        <div>
-<h1> Hello {name} a.k.a {heroName}</h1>
 
-        </div>
-
-    ) 
-
-}
-export default Greet;
-// export const Greet = () => <h1> Hello Amin</h1>  
 // tepadagi holatda App.js da Greetni nomini o'zgartira olmaysan
--- introduction/hello-world/src/components/Message.js
+++ introduction/hello-world/src/components/Message.js
@@ -3,22 +3,22 @@ import React, {Component} from 'react';
 class Message extends Component {
     constructor(){
         super()
-        this.state = {
-            message: "Welcome visitor"
+        this.state={
+            message: 'Welcome visitor'
         }
     }
     changeMessage(){
         this.setState({
-            message: "Thanks for subsribing"
+            message: "thanks for subscription"
         })
     }
     render(){
         return (
             <div>
-         <h1>{this.state.message}</h1>
-         <button onClick={()=> this.changeMessage()}>Subscribe</button>
+          <h1>{this.state.message}</h1>
+          <button onClick={()=> this.changeMessage()}>Subscribe</button>
             </div>
-        )
+        ) 
     }
 }
 
