6e025d6434f146fb2818d0728b8154433861635f
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2024-02-10 03:11:52 +0100

    props and states, event handling, binding event handlers, methods as props, conditional rendering, list rendering

--- introduction/review/src/App.js
+++ introduction/review/src/App.js
@@ -6,6 +6,12 @@ import Hello from './components/Hello';
 import Hellow from './components/HelloHTML';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 class App extends Component {
   render(){
     return (
@@ -30,7 +36,13 @@ class App extends Component {
     <Hellow/> */}
     {/* <Counter/> */}
     
-    <Greet name="Hg" heroName="kl"/>
+    {/* <Greet name="Hg" heroName="kl"/> */}
+    {/* <FunctionClick/>
+    <ClassClick/> */}
+    {/* <EventBind/> */}
+    {/* <ParentComponent/> */}
+    {/* <UserGreeting/> */}
+    <NameList/>
       </div>
     )
   }
