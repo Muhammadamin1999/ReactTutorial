333b79ef00f4dba85ea4d63818903af853c5963c
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2024-02-11 17:00:39 +0100

    styling, form

--- introduction/review/src/App.js
+++ introduction/review/src/App.js
@@ -12,6 +12,11 @@ import EventBind from './components/EventBind';
 import ParentComponent from './components/ParentComponent';
 import UserGreeting from './components/UserGreeting';
 import NameList from './components/NameList';
+import Stylesheet from './components/Stylesheet';
+import Style from './components/Style';
+import './appStyles.css'
+import styles from './appStyles.module.css'
+import Form from './components/Form';
 class App extends Component {
   render(){
     return (
@@ -42,7 +47,12 @@ class App extends Component {
     {/* <EventBind/> */}
     {/* <ParentComponent/> */}
     {/* <UserGreeting/> */}
-    <NameList/>
+    {/* <NameList/> */}
+    {/* <Stylesheet primary={true}/> */}
+    {/* <Style/> */}
+    {/* <h1 className='error'>Error</h1>
+    <h1 className={styles.success}>Success</h1> */}
+    <Form/>
       </div>
     )
   }
