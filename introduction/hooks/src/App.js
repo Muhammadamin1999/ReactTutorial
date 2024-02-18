be36b1bbe5a33ea3e0d305ce720d572a9180d0c9
Muhammadamin1999 <66532689+Muhammadamin1999@users.noreply.github.com>
2024-02-18 23:08:28 +0100

    hooks, useEffect, useState

---
+++ introduction/hooks/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne1 from './components/HookCounterOne1';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
+import HookMouse1 from './components/HookMouse1';
+import MouseContainer from './components/MouseContainer';
+function App() {
+  return (
+    <div className="App">
+     {/* <ClassCounter/> */}
+     {/* <HookCounter/> */}
+     {/* <HookCounterTwo/> */}
+     {/* <HookCounterThree/> */}
+     {/* <HookCounterFour/> */}
+     {/* <ClassCounterOne/> */}
+     {/* <HookCounterOne1/> */}
+     {/* <ClassMouse/> */}
+     {/* <HookMouse/> */}
+     {/* <HookMouse1/> */}
+     <MouseContainer/>
+
+    </div>
+  );
+}
+
+export default App;
