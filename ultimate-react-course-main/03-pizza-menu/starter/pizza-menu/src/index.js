d309d11b711ae72d453170a6b812564491355448
Muhammadamin1999 <babyboyfrank1999@gmail.com>
2024-02-28 22:11:58 +0100

    pizza menu project with components and props

--- ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
+++ ultimate-react-course-main/03-pizza-menu/starter/pizza-menu/src/index.js
@@ -1,17 +1,108 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+function App(){
+  return (
+    <div>
+      <h1>Hello React !</h1>
+
+    </div>
+  )
+}
+function Header(){
+return(
+  <div>
+
+  </div>
+)
+}
+
+function Menu(){
+return(
+<main className='menu'>
+<h2>Our Menu</h2>
+<Pizza
+name='Pizza Spinaci'
+ingredients = "Tomato, mozarella, spinach, and ricotta cheese"
+photoName = 'pizzas/spinaci.jpg'
+price='10'
+/>
+</main>
+)
+}
+function Pizza(props){
+
+  return (
+    <div>
+      <img src={props.photoName} alt={props.name}/>
+      <h3>{props.name}</h3>
+      <p>{props.ingredients}</p>
+    </div>
+  );
+}
+function Footer(){
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App/>
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
