66353d5c7e2161f9cf7451c25c404385fc4c68cc
Muhammadamin1999 <babyboyfrank1999@gmail.com>
2024-03-07 23:27:16 +0100

    use State

--- ultimate-react-course-main/04-steps/starter/steps/src/App.js
+++ ultimate-react-course-main/04-steps/starter/steps/src/App.js
@@ -1,4 +1,5 @@
 
+import { useState } from 'react';
 import './App.css';
 const messages = [
   "Learn React ⚛️",
@@ -7,9 +8,23 @@ const messages = [
 ];
 
 function App() {
-  const step = 2;
+  
+const [step, setStep] = useState(1)
+const [isOpen, setIsOpen ] = useState(true)
+
+
+  function handlePrevious(){
+    if(step > 1) setStep((s)=> s - 1)
+  }
+  function handleNext(){
+      if(step < 3) setStep((s)=> s + 1);
+  }
   return (
-    <div className='steps'>
+    <>
+   <button className='close' onClick={()=> setIsOpen((is)=>!is)}>&times;</button>
+    
+    { isOpen && (
+      <div className='steps'>
       <div className='numbers'>
       <div className={`${step >= 1 ? 'active' : ''}`}>1</div>
       <div className={`${step >= 2 ? 'active' : ''}`}>2</div>
@@ -18,12 +33,15 @@ function App() {
     <p className='message'>Step {step}: {messages[step-1]}</p>
 
     <div className='buttons'>
-      <button style={{backgroundColor: "#7950f2", color: '#fff'}}>
+      <button style={{backgroundColor: "#7950f2", color: '#fff'}} onClick={handlePrevious}>
         Previous
       </button>
-      <button style={{backgroundColor: "#7950f2", color: '#fff'}}>Next</button>
+      <button style={{backgroundColor: "#7950f2", color: '#fff'}} onClick={handleNext}>Next</button>
     </div>
     </div>
+    )
+    }
+    </>
   );
 }
 
