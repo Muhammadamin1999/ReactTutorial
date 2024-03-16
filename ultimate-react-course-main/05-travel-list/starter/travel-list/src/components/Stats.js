e7828f2c2cd118a28dec72e73dc8a19b7638e92d
Muhammadamin1999 <babyboyfrank1999@gmail.com>
2024-03-16 23:54:44 +0100

    buliding travel list app using components props and useState

---
+++ ultimate-react-course-main/05-travel-list/starter/travel-list/src/components/Stats.js
@@ -0,0 +1,9 @@
+export default function Stats(){
+    return(
+   <footer className="stats">
+      <em>
+        You have X times on your list and you already packed x (x%)
+      </em>
+    </footer>
+    ) 
+   }
